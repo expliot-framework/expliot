81eb4428ba88ed5f92fef51ae321cdf00fc57a54
Fabian Affolter <mail@fabian-affolter.ch>
2019-07-26 15:01:51 +0000

    Rename and move efconsole, use 'entry_points'

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-#
-#
-# expliot - Internet Of Things Security Testing and Exploitation Framework
-# 
-# Copyright (C) 2018  Aseem Jakhar
-#
-# Email:   aseemjakhar@gmail.com
-# Twitter: @aseemjakhar
-#
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
+#!/usr/bin/env python3
+"""Setup script for EXPLIoT."""
 import os
 from setuptools import setup, find_packages
 
@@ -24,8 +7,8 @@ from expliot import Expliot
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(os.path.join(here, "README.md"), encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name="expliot",
@@ -37,10 +20,21 @@ setup(
     description="Expliot - IoT security testing and exploitation framework",
     long_description=long_description,
     packages=find_packages(),
-    scripts=["bin/expliot"],
-    install_requires=["cmd2>=0.9.13", "paho-mqtt>=1.3.1", "coapthon3>=1.0.1", "bluepy>=1.1.4", "pyserial>=3.4",
-                      "pyparsing>=2.2.0", "pycrypto>=2.6.1", "pymodbus>=1.5.2", "python-can>=2.1.0",
-                      "pyspiflash>=0.5.2", "pyi2cflash>=0.1.1", "pynetdicom>=1.2.0"],
+    entry_points={"console_scripts": ["expliot=expliot.expliot:EfCli.main"]},
+    install_requires=[
+        "bluepy>=1.1.4",
+        "cmd2>=0.9.13",
+        "coapthon3>=1.0.1",
+        "paho-mqtt>=1.3.1",
+        "pycrypto>=2.6.1",
+        "pyi2cflash>=0.1.1",
+        "pymodbus>=1.5.2",
+        "pynetdicom>=1.2.0",
+        "pyparsing>=2.2.0",
+        "pyserial>=3.4",
+        "pyspiflash>=0.5.2",
+        "python-can>=2.1.0",
+    ],
     python_requires=">=3.5",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -48,8 +42,7 @@ setup(
         "Intended Audience :: Information Technology",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Natural Language :: English"
-        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English" "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Security",
