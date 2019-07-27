8eab901bd5774db25c84226858b943217b3bdbf8
Fabian Affolter <fabian@affolter-engineering.ch>
2019-07-27 19:50:11 +0200

    Update style, variable names and docstrings

--- expliot/core/tests/testsuite.py
+++ expliot/core/tests/testsuite.py
@@ -1,74 +1,57 @@
-#
-#
-# expliot - Internet Of Things Security Testing and Exploitation Framework
-# 
-# Copyright (C) 2018  Aseem Jakhar
-#
-# Email:   aseemjakhar@gmail.com
-# Twitter: @aseemjakhar
-#
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-
-
+"""Test suite handler."""
+import importlib
 import inspect
 import pkgutil
-import importlib
-
-
 import sys
-sys.path.append("..")
 
 from expliot.core.tests.test import Test
 
+sys.path.append("..")
+
+
 class TestSuite(dict):
     """
-    TestSuite class inherits from dict and stores all the test cases
+    Test Suite class inherits from dict and stores all the test cases
     from plugins package name specified in __init__()
     """
 
     testcls = [Test]
 
-    def __init__(self, pkgname='expliot.plugins'):
+    def __init__(self, pkgname="expliot.plugins"):
         """
-        Initialize the test suite with the plugins in the package
-        :param pkgname: The package to load test case plugins from or 'expliot.plugins' if None specified
+        Initialize the test suite with the plugins in the package.
+
+        :param pkgname: The package to load test case plugins from or
+                        'expliot.plugins' if None specified
         """
-        #self.tmap = {}
         self.import_plugins(pkgname)
 
     def import_plugins(self, pkgname):
         """
-        Imports all tests from the specified Package into a dict
+        Import all tests from the specified Package into a dict.
+
         :param pkgname: The package to load all test case plugins from
-        :return: void
+        :return:
         """
-        pkgs = [pkgname]
+        packages = [pkgname]
         # Import from all subpackages recursively
-        while len(pkgs) > 0:
-            pkg = pkgs.pop()
+        while len(packages) > 0:
+            pkg = packages.pop()
             pmod = importlib.import_module(pkg)
-            prefix =  pmod.__name__ + "."
-            for finder, name, ispkg in pkgutil.iter_modules(pmod.__path__, prefix):
-                if ispkg:
-                    pkgs.append(name)
+            prefix = "{}.".format(pmod.__name__)
+            for finder, name, is_pkg in pkgutil.iter_modules(pmod.__path__, prefix):
+                if is_pkg:
+                    packages.append(name)
                 else:
                     mod = importlib.import_module(name)
-                    for tname, klass in inspect.getmembers(mod):
-                        if inspect.isclass(klass) and issubclass(klass, Test) and klass not in TestSuite.testcls:
-                            t = klass()
-                            self[t.id] = {"class":klass, "summary":t.summary}
-
-
-#if __name__ == "__main__":
-#    ts = TestSuite()
-#    t = ts.get('ssample', None)
-#    print(t)
+                    for test_name, test_class in inspect.getmembers(mod):
+                        if (
+                            inspect.isclass(test_class)
+                            and issubclass(test_class, Test)
+                            and test_class not in TestSuite.testcls
+                        ):
+                            test = test_class()
+                            self[test.id] = {
+                                "class": test_class,
+                                "summary": test.summary,
+                            }
