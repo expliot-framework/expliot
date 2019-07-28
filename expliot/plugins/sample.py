1a066b1ab01d29a45e8e5b3f8cacb658a9748ad1
Fabian Affolter <fabian@affolter-engineering.ch>
2019-07-28 21:39:05 +0200

    Update/add docstrings

--- expliot/plugins/sample.py
+++ expliot/plugins/sample.py
@@ -1,57 +1,64 @@
-#
-#
-# expliot - Internet Of Things Security Testing and Exploitation Framework
-# 
-# Copyright (C) 2018  Aseem Jakhar
-#
-# Email:   aseemjakhar@gmail.com
-# Twitter: @aseemjakhar
-#
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-
-
+"""Sample test/plugin as demo."""
 from expliot.core.tests.test import Test, TCategory, TTarget, TLog
 
-class Sample(Test):
-    def __init__(self):
-        super().__init__(name     = "Sample",
-                         summary  = "Sample Summary",
-                         descr    = "Sample Description",
-                         author   = "Sample author",
-                         email    = "email@example.com",
-                         ref      = ["https://example.com", "https://example.dom"],
-                         category = TCategory(TCategory.COAP, TCategory.SW, TCategory.EXPLOIT),
-                         target   = TTarget(TTarget.GENERIC, TTarget.GENERIC, TTarget.GENERIC))
 
-        self.argparser.add_argument("-r", "--rhost", required=True, help="IP address of the target")
-        self.argparser.add_argument("-p", "--rport", default=80, type=int, help="Port number of the target. Default is 80")
-        self.argparser.add_argument("-v", "--verbose", action="store_true", help="show verbose output")
+class Sample(Test):
+    """Test class for the sample."""
+
+    def __init__(self):
+        """Initialize the test."""
+        super().__init__(
+            name="Sample",
+            summary="Sample Summary",
+            descr="Sample Description",
+            author="Sample author",
+            email="email@example.com",
+            ref=["https://example.com", "https://example.dom"],
+            category=TCategory(TCategory.COAP, TCategory.SW, TCategory.EXPLOIT),
+            target=TTarget(TTarget.GENERIC, TTarget.GENERIC, TTarget.GENERIC),
+        )
+
+        self.argparser.add_argument(
+            "-r", "--rhost", required=True, help="IP address of the target"
+        )
+        self.argparser.add_argument(
+            "-p",
+            "--rport",
+            default=80,
+            type=int,
+            help="Port number of the target. Default is 80",
+        )
+        self.argparser.add_argument(
+            "-v", "--verbose", action="store_true", help="show verbose output"
+        )
 
     def pre(self):
+        """Run before the test."""
         TLog.generic("Enter {}.pre()".format(self.id))
         # Only implement this if you need to do some setup etc.
         TLog.generic("Exit {}.pre()".format(self.id))
 
     def post(self):
+        """Run after the test."""
         TLog.generic("Enter {}.post()".format(self.id))
         # Only implement this if you need to do some cleanup etc.
         TLog.generic("Exit {}.post()".format(self.id))
 
     def execute(self):
-        TLog.generic("Sending request to server({}) on port({})".format(self.args.rhost, self.args.rport))
+        """Execute the test."""
+        TLog.generic(
+            "Sending request to server({}) on port({})".format(
+                self.args.rhost, self.args.rport
+            )
+        )
         TLog.trydo("Searching imaginary database")
         TLog.success("Found matching entry in db - ({})".format("FooEntry"))
         snd = "GET / HTTP/1.1"
-        TLog.generic("Sending command to server ({}) on port ({})".format(self.args.rhost, self.args.rport))
+        TLog.generic(
+            "Sending command to server ({}) on port ({})".format(
+                self.args.rhost, self.args.rport
+            )
+        )
         if self.args.verbose is True:
             TLog.generic("More verbose output. sending payload ({})".format(snd))
         TLog.fail("No response received")
@@ -61,4 +68,3 @@ class Sample(Test):
         self.result.setstatus(passed=False, reason="Server is not vulnerable")
         # Or in case you want the test to fail with whatever exception occured as the reason
         # self.result.exception()
-
