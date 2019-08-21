6a1c92b5eeb0103ab58c096eb5e2919f0cc256b5
Fabian Affolter <fabian@affolter-engineering.ch>
2019-08-21 18:42:28 +0200

    Update/add docstrings

--- expliot/core/protocols/hardware/can/__init__.py
+++ expliot/core/protocols/hardware/can/__init__.py
@@ -5,9 +5,11 @@ from can import Message
 
 class CanBus(Bus):
     """A simple wrapper around python-can Bus class."""
+
     pass
 
 
 class CanMessage(Message):
     """A simple wrapper around python-can Message class."""
+
     pass
