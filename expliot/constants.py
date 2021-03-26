3a9557dd02282eee2ea9a07dba25a2d5a8cb1c44
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-26 21:16:20 +0000

    Prepare for 0.9.7

--- expliot/constants.py
+++ expliot/constants.py
@@ -1,7 +1,7 @@
 """Constants for EXPLIoT."""
 MAJOR_VERSION = 0
 MINOR_VERSION = 9
-PATCH_VERSION = 6
+PATCH_VERSION = 7
 
 __short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
